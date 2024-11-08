2722f7cd943a37b032bc9fc687fa36711f5a1146
Marcin Olszewski <cinula@gmail.com>
2024-11-08 10:59:59 +0000

    adding heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bdstobnupf(x!4v=!2p4k33d5x7utg5%+0&l$^@kk8(o)z&=80'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-cinula-djangoproject-zex59cw6sdw.ws-eu116.gitpod.io']
+ALLOWED_HOSTS = ['8000-cinula-djangoproject-zex59cw6sdw.ws-eu116.gitpod.io',
+                '.herokuapp.com']
 
 
 # Application definition
